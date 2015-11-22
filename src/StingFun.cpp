786cc7d2170c44d9d5e32dc092824dba30c956ac
mahesh <maheswarreddy.utukuru@gamil.com>
2015-11-22 13:26:07 +0100

    added a class

---
+++ src/StingFun.cpp
@@ -0,0 +1,23 @@
+/*
+ * StingFun.cpp
+ *
+ *  Created on: 22-Nov-2015
+ *      Author: mahesh
+ */
+
+#include "StingFun.h"
+
+StingFun::StingFun() {
+	// TODO Auto-generated constructor stub
+
+}
+
+StingFun::~StingFun() {
+	// TODO Auto-generated destructor stub
+}
+
+void StingFun::ShowName(std::string name){
+
+	std::cout<<" name is : "<<name<<"\n";
+}
+
---
+++ src/StingFun.h
@@ -0,0 +1,18 @@
+/*
+ * StingFun.h
+ *
+ *  Created on: 22-Nov-2015
+ *      Author: mahesh
+ */
+
+#ifndef STINGFUN_H_
+#define STINGFUN_H_
+
+class StingFun {
+public:
+	StingFun();
+	virtual ~StingFun();
+	void ShowName(std::string name);
+};
+
+#endif /* STINGFUN_H_ */
